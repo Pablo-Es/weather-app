b0b0b5d54ec8689cc5407cf2840b23dc02d12987
Pablo-Es <szyposz@gmail.com>
2018-09-24 22:52:37 +0200

    create map and locations panel

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <v-container fluid>
+      <v-layout row wrap>
+        <map-view/>
+        <accordion/>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+import MapView from "./components/MapView";
+import Accordion from "./components/Accordion";
+export default {
+  name: 'App',
+  components: {Accordion, MapView}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MapView.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-flex md8>
+    <v-layout row wrap>
+      <v-flex md10>
+        <h2 class=" text-sm-left">World map</h2>
+      </v-flex>
+      <v-flex md2>
+        <v-select
+          :items="items"
+          label="Standard"
+          text-sm-right
+        ></v-select>
+      </v-flex>
+    </v-layout>
+    <v-layout>
+      <v-flex xs12>
+        <GmapMap
+          :center="{lat:10, lng:10}"
+          :zoom="7"
+          map-type-id="terrain"
+          style="width: 100%; height: 80vh"
+        >
+          <GmapMarker
+            :key="index"
+            v-for="(m, index) in markers"
+            :position="m.position"
+            :clickable="true"
+            :draggable="true"
+            @click="center=m.position"
+          />
+        </GmapMap>
+      </v-flex>
+    </v-layout>
+
+
+  </v-flex>
+</template>
+
+<script>
+    export default {
+      name: "MapView",
+      data() {
+        return {
+          center: {
+            lat: 10.0,
+            lng: 10.0
+          },
+          markers: [{
+            position: {
+              lat: 10.0,
+              lng: 10.0
+            }
+          }, {
+            position: {
+              lat: 11.0,
+              lng: 11.0
+            }
+          }]
+
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
