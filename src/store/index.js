690c2dbe7892de222d5526ef97cedc062ab3b8d6
Pablo-Es <szyposz@gmail.com>
2018-10-23 21:56:20 +0200

    added store data to LocalStorage

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,19 @@
 import map from './modules/map';
 import Vuex from "vuex";
 import Vue from 'vue'
+import VuexPersist from 'vuex-persist'
 
 Vue.use(Vuex);
-
+const vuexPersist = new VuexPersist({
+  key: 'weather-app',
+  storage: window.localStorage
+})
 const store = new Vuex.Store({
   'modules': {
     'map': map,
 
   },
+  plugins: [vuexPersist.plugin]
 })
 
 window.store = store;
