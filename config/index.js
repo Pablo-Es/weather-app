8b6b3f76d89b8159b4660add36c146ef0fa2d7ab
Pablo-Es <szyposz@gmail.com>
2018-10-29 23:07:35 +0100

    changed path

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/weather-app/',
+    assetsPublicPath: '/weather-app',
 
     /**
      * Source Maps
